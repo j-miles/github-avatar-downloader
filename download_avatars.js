f81ef666efdb523b32f3833765365a085ca35bfc
Jackie <jaxmiles@gmail.com>
2017-03-13 23:46:53 +0000

    added github token, user and URL builder

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
+var GITHUB_USER = "j-miles";
+var GITHUB_TOKEN = "36c102eac51a57e975b9c4830ce56f8d3d2e6ea0";
+
 var request = require('request');
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+
+});
+
+
+

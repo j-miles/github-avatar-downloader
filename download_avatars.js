c6d332eb66692bb73174ee0a6e4d442015033f97
Jackie <jaxmiles@gmail.com>
2017-03-14 02:50:27 +0000

    finished step 8

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,50 @@
-var GITHUB_USER = "j-miles";
-var GITHUB_TOKEN = "36c102eac51a57e975b9c4830ce56f8d3d2e6ea0";
-
 var request = require('request');
+var fs = require('fs');
+var link = 'https://api.github.com/repos/jquery/jquery/contributors';
+var GITHUB_USER = 'j-miles';
+var GITHUB_TOKEN = '332851383fea7493037f0bcc27581247ef63230fe';
+var options = {
+ url: link,
+ headers: {
+   'User-Agent': 'j-miles'
+ }
+};
 
-
-console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+ var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+ request(options, function(error, response, body) {
+   if(error) {
+     console.log('Error: ' + error);
+     throw error;
+   }
+   if(response.statusCode === 200){
+     console.log('OK');
+   }
+   if(body){
+     var parsed = JSON.parse(body);
+     cb(null, parsed);
+   }
+ })
 }
 
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+   .on('error', function(error){
+     console.log('Error!');
+     throw error;
+   })
+   .on('response', function(response){
+     console.log(response.headers);
+   })
+   .pipe(fs.createWriteStream(filePath));
+ }
+
+
+getRepoContributors("jquery", "jquery", function(err, response) {
+ for(var i in response){
+  downloadImageByURL(response[i]['avatar_url'], 'avatars/'+response[i]['login']+'.jpg');
+ }
 });
-
-
-
